00733d69e84ff8f25e76c25c57ccbb4146725ebb
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-03-22 19:28:09 +0300

    created the Contact model class

--- bin/src/Portfolio/Contact/models.py
+++ bin/src/Portfolio/Contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Contact(models.Model):
+    fullName = models.CharField(max_length=100)
+    email = models.EmailField(max_length=70)
+    subject = models.CharField(max_length=120)
+    message = models.TextField()
+    datestamp = models.DateTimeField(auto_now_add=True)
