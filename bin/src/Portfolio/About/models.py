f875f1e2df7a8d36f08da7ac844cc3d605715dcb
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-03-22 19:18:14 +0300

    created the About model classes

--- bin/src/Portfolio/About/models.py
+++ bin/src/Portfolio/About/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class About(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='', null=True)
+    description = models.TextField()
+    resume = models.FileField(upload_to='', blank=True, null=True)
+    featured = models.BooleanField(default=False)
+    datestamp = models.DateTimeField(auto_now_add=True)
