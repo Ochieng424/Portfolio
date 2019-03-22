b41a0e4916cd2b0f3a191920c9ab302b96f56058
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-03-22 19:59:42 +0300

    created a Pages app that manages all the other apps views

--- bin/src/Portfolio/Pages/admin.py
+++ bin/src/Portfolio/Pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.site.site_header = 'Portfolio Admin'
+admin.site.unregister(Group)
--- bin/src/Portfolio/Pages/views.py
+++ bin/src/Portfolio/Pages/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from About.models import About
+from Works.models import Work
+from Contact.form import ContactForm
+from Skill.models import MySkill
+
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    obj = About.objects.all()
+    context = {
+        'object': obj
+    }
+    return render(request, "home.html", context)
+
+
+def work_view(request):
+    obj = Work.objects.all()
+    context = {
+        'object': obj
+    }
+    return render(request, "work.html", context)
+
+
+def contact_view(request, *args, **kwargs):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        if form.save():
+            messages.success(request, "Thanks for Contacting Me")
+            form = ContactForm()
+        else:
+            messages.error(request, "An error occurred.. Try again")
+
+    context = {
+        'form': form
+    }
+    return render(request, "contact.html", context)
+
+
+def skill_view(request, *args, **kwargs):
+    obj = MySkill.objects.all()
+    context = {
+        'object': obj
+    }
+    return render(request, "skill.html", context)
