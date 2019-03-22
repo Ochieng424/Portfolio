a2eb48d6d14dd99f3bc96a6cd108077cdca1e1e9
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-03-22 19:46:55 +0300

    Registered contacts model to admin.py

--- bin/src/Portfolio/Contact/admin.py
+++ bin/src/Portfolio/Contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Contact
+
 
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('fullName', 'email', 'subject', 'message')
+    list_display_links = None
+    sortable_by = ('datestamp',)
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(Contact, ContactAdmin)
