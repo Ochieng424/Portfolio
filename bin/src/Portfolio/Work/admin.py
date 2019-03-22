005f2caf63791e0bb7b42c17f70f71ac7c8ac54e
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-03-22 20:48:22 +0300

    Finishing up

--- bin/src/Portfolio/Work/admin.py
+++ bin/src/Portfolio/Work/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Work
+
 
 # Register your models here.
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'datestamp')
+
+
+admin.site.register(Work, WorkAdmin)
--- bin/src/Portfolio/Work/models.py
+++ bin/src/Portfolio/Work/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Work(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='')
+    live_project_link = models.URLField(max_length=100, blank=True)
+    source_code_link = models.URLField(max_length=100)
+    datestamp = models.DateTimeField(auto_now_add=True)
