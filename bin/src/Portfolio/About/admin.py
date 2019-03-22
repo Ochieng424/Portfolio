2063411323e7c88c7b71ad39417510393c321adc
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-03-22 19:19:18 +0300

    registered the About model to the admin

--- bin/src/Portfolio/About/admin.py
+++ bin/src/Portfolio/About/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import About
+
+
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'featured')
+
+
+admin.site.register(About, AboutAdmin)
