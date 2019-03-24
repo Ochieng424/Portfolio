89ab9b7eca041b10c57e77175b5782cc258676c7
[Ochieng424] <[ochiengderrick424@gmail.com]>
2019-03-24 18:49:36 +0300

    :zap: Improved Performance

--- bin/src/Portfolio/About/models.py
+++ bin/src/Portfolio/About/models.py
@@ -12,6 +12,7 @@ class About(models.Model):
     image_url = models.CharField(max_length=255, null=True, blank=True)
     description = models.TextField()
     resume = models.FileField(upload_to='', blank=True, null=True)
+    resume_url = models.CharField(max_length=255, null=True, blank=True)
     featured = models.BooleanField(default=False)
     datestamp = models.DateTimeField(auto_now_add=True)
 
@@ -20,10 +21,11 @@ class About(models.Model):
 def about_post_save(**kwargs):
     print(kwargs)
     instance = kwargs.get("instance")
-    upload_file(instance.id, instance.image.path)
+    upload_image(instance.id, instance.image.path)
+    upload_resume(instance.id, instance.resume.path)
 
 
-def upload_file(id, file_path):
+def upload_image(id, file_path):
     files = {
         'upload': open(file_path, 'rb')
     }
@@ -33,5 +35,19 @@ def upload_file(id, file_path):
         print("RESPONSE: {0}".format(response.json()))
         response_data = response.json()
         About.objects.filter(id=id).update(image_url=response_data['file'])
+    else:
+        print("ERROR: {0}".format(response.json()))
+
+
+def upload_resume(id, file_path):
+    files = {
+        'upload': open(file_path, 'rb')
+    }
+
+    response = requests.post(url="https://projects.cognition.co.ke/file-upload/index.php", files=files)
+    if response.status_code == 201:
+        print("RESPONSE: {0}".format(response.json()))
+        response_data = response.json()
+        About.objects.filter(id=id).update(resume_url=response_data['file'])
     else:
         print("ERROR: {0}".format(response.json()))
--- bin/src/Portfolio/Work/models.py
+++ bin/src/Portfolio/Work/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import requests
 
 
 # Create your models here.
@@ -6,6 +9,28 @@ class Work(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     image = models.ImageField(upload_to='')
+    image_url = models.CharField(max_length=255, null=True, blank=True)
     live_project_link = models.URLField(max_length=100, blank=True)
     source_code_link = models.URLField(max_length=100)
     datestamp = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save, sender=Work)
+def work_post_save(**kwargs):
+    print(kwargs)
+    instance = kwargs.get("instance")
+    upload_image(instance.id, instance.image.path)
+
+
+def upload_image(id, file_path):
+    files = {
+        'upload': open(file_path, 'rb')
+    }
+
+    response = requests.post(url="https://projects.cognition.co.ke/file-upload/index.php", files=files)
+    if response.status_code == 201:
+        print("RESPONSE: {0}".format(response.json()))
+        response_data = response.json()
+        Work.objects.filter(id=id).update(image_url=response_data['file'])
+    else:
+        print("ERROR: {0}".format(response.json()))
